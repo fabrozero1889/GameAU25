ae8cfe98a31758d5897b7261047cd9692aab08ab
fabrozero1889 <fabro.zero@gmail.com>
2025-09-06 15:29:43 -0300

    Pelota suena y no se corta el audio

--- Assets/Scripts/DestroyBall.cs
+++ Assets/Scripts/DestroyBall.cs
@@ -1,17 +1,23 @@
+using DoorScript;
 using UnityEngine;
 
 public class DestroyBall : MonoBehaviour
 {
+    [SerializeField] AudioSource BallBounce;
+    [SerializeField] AudioSource Electricity;
+
     private void OnCollisionEnter(UnityEngine.Collision collision)
     {
         if (collision.gameObject.CompareTag("kill"))
             Destroy(gameObject);
+      
 
     }
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("kill"))
             Destroy(gameObject);
+        AudioSource.PlayClipAtPoint(Electricity.clip, transform.position);
         
     }
 }
