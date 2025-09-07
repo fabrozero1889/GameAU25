cc90eddef38bb5c83869a3d4d0a3716e6040f2ec
fabrozero1889 <fabro.zero@gmail.com>
2025-09-07 18:49:10 -0300

    Puerta abrió

--- Assets/Scripts/DestroyBall.cs
+++ Assets/Scripts/DestroyBall.cs
@@ -5,19 +5,27 @@ public class DestroyBall : MonoBehaviour
 {
     [SerializeField] AudioSource BallBounce;
     [SerializeField] AudioSource Electricity;
+    [SerializeField] AudioSource OpenDoor;
+    [SerializeField] GameObject Door;
 
     private void OnCollisionEnter(UnityEngine.Collision collision)
     {
         if (collision.gameObject.CompareTag("kill"))
             Destroy(gameObject);
-      
+        
+            
+
+
 
     }
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("kill"))
             Destroy(gameObject);
+          Door.GetComponent<Door>().open = true;
+
         AudioSource.PlayClipAtPoint(Electricity.clip, transform.position);
+        AudioSource.PlayClipAtPoint(OpenDoor.clip, transform.position);
         
     }
 }
